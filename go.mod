059a8d661e7890335bce637acf25b337a6249b40
Andre Santos (AB) <afsantos@stone.com.br>
2024-02-14 21:54:25 -0300

    feat: http server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dexfs/container-server-go
+
+go 1.22.0
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+
+func main() {
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(rw, "Olá mundo\n")
+	})
+
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
