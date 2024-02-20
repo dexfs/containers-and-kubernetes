d2fcbb735ac7e045aee7b14c00792664dc08861d
Andre Santos (AB) <afsantos@stone.com.br>
2024-02-19 21:04:50 -0300

    feat: saving data on redis

--- main.go
+++ main.go
@@ -1,16 +1,47 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"github.com/redis/go-redis/v9"
 	"log"
 	"net/http"
 )
 
+var ctx = context.Background()
 
-func main() {
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(rw, "Olá mundo\n")
+type Server struct {
+	redis *redis.Client
+}
+
+func (s *Server) Run() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodPost:
+			err := s.redis.Set(ctx, "name", r.URL.Query().Get("name"), 0).Err()
+			if err != nil {
+				log.Println("ERROR: ", err)
+			}
+
+		case http.MethodGet:
+			name, err := s.redis.Get(ctx, "name").Result()
+			if err != nil {
+				log.Println("ERROR: ", err)
+			}
+
+			w.Write([]byte(name))
+		}
 	})
 
 	log.Fatal(http.ListenAndServe(":8000", nil))
+}
+
+func main() {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "redis:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	s := Server{rdb}
+	s.Run()
 }
